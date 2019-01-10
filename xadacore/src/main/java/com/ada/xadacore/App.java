81aa5e84f23fedbbb1aac43eb675fb8b723015d4
Master_Zen <daalpiva@gmail.com>
2019-01-10 18:51:44 -0500

    Arrange 2

--- xadacore/src/main/java/com/ada/xadacore/App.java
+++ xadacore/src/main/java/com/ada/xadacore/App.java
@@ -1,15 +1,20 @@
 package com.ada.xadacore;
 
+import com.ada.fics.accessdata.persistence.mapper.GestionadorEmpresasMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.ada.fics.accessdata.persistence","com.ada.fics.xcomp.business"})
+@ComponentScan("com.ada")
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        GestionadorEmpresasMapper gestionadorEmpresasMapper = null;
+        ConfigurableApplicationContext abc = SpringApplication.run(App.class, args);
+        abc.getBean(gestionadorEmpresasMapper.getClass() );
+        abc.close();
     }
 
 }
